7a9dcf94e2f72726be5e29adbd655e34443312af
rcambianica <r.cambianica@campus.unimib.it>
2022-11-24 16:19:08 +0100

    altre prove con strings.xml

--- app/src/main/java/com/example/foodalchemy/MainActivity.java
+++ app/src/main/java/com/example/foodalchemy/MainActivity.java
@@ -3,14 +3,12 @@ package com.example.foodalchemy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-//commento 1
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //aggiunto commento da locale
-        //aggiunto commento da remoto 2
     }
 }
