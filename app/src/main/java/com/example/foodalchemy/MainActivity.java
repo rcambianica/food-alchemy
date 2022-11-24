c34fdef55bfde07b31383ab1edcb6ec386be25f2
RiccardoCambianica <102529585+RiccardoCambianica@users.noreply.github.com>
2022-11-24 13:12:11 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/foodalchemy/MainActivity.java
+++ app/src/main/java/com/example/foodalchemy/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //aggiunto commento da locale
+        //aggiunto commento da remoto 2
     }
 }
